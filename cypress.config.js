efc0ac89711221695c2e96b13a374e7000649c4c
Kristine Berke <kristine.berke@gmail.com>
2024-07-16 14:31:53 +0300

    Updated Cypress configuration for version 10.0.0

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  baseUrl: 'https://localhost:8080',
+  baseUrl: "https://localhost:8080",
   viewportWidth: 1280,
   viewportHeight: 720,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
