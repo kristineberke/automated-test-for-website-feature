d8ad51261877aaabd9ea299b8745e142f62978b6
Kristīne Berķe <kristine.berke@gmail.com>
2024-07-16 14:38:12 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-<<<<<<< HEAD
-  baseUrl: "https://localhost:8080",
-=======
   e2e: {
     baseUrl: 'https://localhost:8080',  // Adjust URL as per your setup
   },
->>>>>>> 2e9fa324448a95f8064a9c940ef478d3bfc12a9b
   viewportWidth: 1280,
   viewportHeight: 720,
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  // Any other configurations...
 });
