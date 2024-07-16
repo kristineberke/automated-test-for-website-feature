d8b79cd11724a6dd04bb192164d774a571fbf0dd
Kristīne Berķe <kristine.berke@gmail.com>
2024-07-16 19:06:34 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://localhost:8080',
+    baseUrl: 'http://localhost:8080',
   },
   viewportWidth: 1280,
   viewportHeight: 720,
